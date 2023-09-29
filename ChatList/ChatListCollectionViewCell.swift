6eb2dff6a62064b267f3ae2995558c5984c01d30
tony-yun <lifeyun24@naver.com>
2023-09-29 22:16:14 +0900

    date formatter change and label line change from 1 to 2

--- ChatList/ChatListCollectionViewCell.swift
+++ ChatList/ChatListCollectionViewCell.swift
@@ -17,7 +17,23 @@ class ChatListCollectionViewCell: UICollectionViewCell {
         thumbnail.image = UIImage(named: chat.name)
         nameLabel.text = chat.name
         chatLabel.text = chat.chat
-        dateLabel.text = chat.date
+        dateLabel.text = formattedDateString(dateString: chat.date)
+    }
+    
+    func formattedDateString(dateString: String) -> String {
+        
+        // String -> Date -> String
+        // 2022-04-01 -> 1/4
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        if let date = formatter.date(from: dateString){
+            formatter.dateFormat = "M/d"
+            return formatter.string(from: date)
+        } else {
+            return ""
+        }
     }
     
 }
