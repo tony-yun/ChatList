0efddf4d8d281d8aa69faa02486bbf172b341145
tony-yun <lifeyun24@naver.com>
2023-09-29 18:27:27 +0900

    collection view cell autolayout settle

---
+++ ChatList/ChatListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListViewController.swift
+//  ChatList
+//
+//  Created by 윤태웅 on 9/29/23.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
